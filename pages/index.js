a16b7654f4b325395e54c90459bf230b1cccbd10
Diego Lucas <lukinha_dna83@hotmail.com>
2021-01-27 04:19:35 -0300

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import BackgroundImage from '../src/components/Quizbg';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width:350px;
+  padding-top:45px;
+  margin: auto;
+  @media screen and (max-width: 500px){
+    margin:auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <BackgroundImage backgroundImage={db.bg}>
+      <QuizContainer>
+          <Widget>
+          <Widget.Header>
+            <Widget.Content>
+                <h1>Quiz Test</h1>
+                <p>
+                Neque porro quisquam est qui dolorem ipsum quia dolor sit ame
+                </p>
+            </Widget.Content>    
+          </Widget.Header>
+          </Widget>
+
+          <Widget>
+            <Widget.Header>
+              <Widget.Content>
+                <h1>Alternativas</h1>
+                <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit ame</p>
+              </Widget.Content>
+            </Widget.Header>
+            
+          </Widget>
+          <Footer />
+      </QuizContainer>
+    </BackgroundImage>
+  )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+// src/components/Footer/index.js
+const FooterWrapper = styled.footer`
+  background-color: #00000070;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  margin-top: 100px;
+  border-radius: 4px; 
+  img {
+    width: 58px;
+    margin-right: 23px;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: .3s;
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+    span {
+      text-decoration: underline;
+      margin-left: 10px;
+    }
+  }
+`;
+
+export default function Footer(props) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FooterWrapper {...props}>
+      <p>
+        {' '}
+        
+        {' '}
+        <a href="https://mydevfolio-01.herokuapp.com/">
+          <span>Feito Por Ureinha</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  );
+}
